03d6576a6b4504b416d818a5649ef192aebb6c94
Aiffah Kiysa Waafi <aiffahwaafi6@gmail.com>
2023-10-13 08:16:37 +0700

    initial commit

---
+++ DSSD/DSSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSSDApp.swift
+//  DSSD
+//
+//  Created by Aiffah Kiysa Waafi on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DSSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
